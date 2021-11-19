11b00dc1c2e6b66b0de7b62ac67d6a125cbb1390
Тимур <timur@MacBook-Pro-Timur.local>
2021-11-19 18:21:04 +0500

    first version

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+        ]
+    }
+    return config;
+}
+
+const babelOpts = preset => {
+    opts = {
+        presets: [
+            '@babel/preset-env',
+        ]
+    }
+    if (preset) {
+        opts.presets.push(preset);
+    }
+    return opts;
+}
+
+module.exports = {
+    entry: {
+        main: ['@babel/polyfill', './src/index.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
+    ],
+    optimization: optimization(),
+    devtool: isDev ? 'source-map' : '',
+    devServer: {
+        port: 5500,
+        hot: isDev
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg|ttf)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelOpts()
+                }
+            },
+            {
+                test: /\.m?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelOpts('@babel/preset-react')
+                }
+            }
+        ],
+    },
+}
